5a8dc2c21d8ccd6abaee8b1461b927374c9baa14
Ray-010 <Ray010@outlook.jp>
2022-06-21 23:28:10 +0900

    change color range

--- detect_color.py
+++ detect_color.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 
-
 def detect_color(file_name, red1_range, red2_range, green_range, save_path_dir):
     img = cv2.imread("./images/" + file_name)
 
-    print(type(img))
+    # 2_19のみ画像を切り取る
+    # (146, 96) (344, 366)
+    # img = img[96:366, 146:344]
 
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # red
@@ -24,9 +25,18 @@ def detect_color(file_name, red1_range, red2_range, green_range, save_path_dir):
 
     frame_mask = frame_mask1 + frame_mask2 + frame_mask3
 
+    # 割合検出
+    red_ratio = np.sum(frame_mask1+frame_mask2)
+    green_ratio = np.sum(frame_mask3)
+    if red_ratio > green_ratio:
+        print("Red")
+    elif green_ratio > red_ratio:
+        print("Green")
+    else:
+        print("Doesn't detect color")
+
     # 論理演算で色検出
     dst = cv2.bitwise_and(img,img, mask=frame_mask)
-
     # cv2.imshow("img",frame_mask)
     cv2.imshow("img", dst)
     if cv2.waitKey(0) & 0xFF == ord("q"):
@@ -39,18 +49,23 @@ file_names = [
     # 青
     "ped_lights_image_2_79.jpg",
     "ped_lights_image_2_19.jpg",
+    "ped_lights_image_2_33.jpg",
+    
     # 赤
     "ped_lights_image_2_71.jpg",
+    "ped_lights_image_2_45.jpg",
+    "ped_lights_image_2_20.jpg",
+
 ]
 
 if __name__ == "__main__":
     # np.array([h色相, s彩度, v明度])
-    red1_range = [[0, 230, 230], [30, 255, 255]]
-    red2_range = [[150, 230, 230], [179, 255, 255]]
+    red1_range = [[0, 150, 150], [30, 255, 255]]
+    red2_range = [[150, 150, 150], [179, 255, 255]]
     green_range = [[30, 150, 150], [90, 255, 255]]
     save_path_dir = "./output3/"
     
-    file_name = file_names[2]
+    file_name = file_names[4]
 
     detect_color(
         file_name = file_name,
